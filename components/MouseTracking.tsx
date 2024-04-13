ed1fbaa4e815a475ebfb198623c12f7bc5d3dc51
Aditya Raj <rajaditya27145@gmail.com>
2024-04-13 21:56:04 +0530

    feat: Cursor effects

--- components/MouseTracking.tsx
+++ components/MouseTracking.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect } from "react";
 
 export const MouseTracking = () => {
   const x = useMotionValue(200);
@@ -8,6 +9,10 @@ export const MouseTracking = () => {
 
   const rotateX = useTransform(y, [0, 400], [45, -45]);
   const rotateY = useTransform(x, [0, 400], [-45, 45]);
+
+  useEffect(() => {
+    console.log(x, y, rotateX, rotateY);
+  }, [x, y, rotateX, rotateY]);
 
   function handleMouse(event: any) {
     const rect = event.currentTarget.getBoundingClientRect();
