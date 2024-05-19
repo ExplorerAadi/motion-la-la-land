36d3661fee201fa483130d3d082cee24e540109d
Aditya Raj <rajaditya27145@gmail.com>
2024-05-20 03:07:03 +0530

    finish v1

--- components/SmartNavbar.tsx
+++ components/SmartNavbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   AnimatePresence,
   motion,
@@ -9,7 +9,6 @@ import {
 } from "framer-motion";
 import Link from "next/link";
 import { classNames } from "../utils";
-import { useOffsets } from "../hooks";
 
 const navItems = [
   {
@@ -109,21 +108,26 @@ export const FloatingNav = ({
 };
 
 export const FloatingContainers = () => {
-  const [visibleIdx, setVisibleIdx] = useState(0);
-  const [allOffsetTops, setAllOffsetTops] = useState<number[]>([]);
+  const [visibleIdx, setVisibleIdx] = useState(-1);
+  const [allOffsetTops, setAllOffsetTops] = useState<Set<number>>(new Set());
   const { scrollY } = useScroll();
   useMotionValueEvent(scrollY, "change", (current) => {
     if (typeof current === "number") {
       const direction = current! - scrollY.getPrevious()!;
+      const allOffsetTopsArr = Array.from(allOffsetTops!);
 
       if (direction > 0) {
-        if (current >= allOffsetTops[0] && current < allOffsetTops[1]) {
-          setVisibleIdx(1);
-        } else if (current >= allOffsetTops[1] && current < allOffsetTops[2]) {
-          setVisibleIdx(2);
-        } else if (current >= allOffsetTops[2]) {
-          setVisibleIdx(3);
-        } else setVisibleIdx(0);
+        allOffsetTopsArr.forEach((offset, idx) => {
+          if (idx === 0 && current >= 90 && current < allOffsetTopsArr[idx + 1]) {
+            setVisibleIdx(idx);
+          } else if (current >= offset && current < allOffsetTopsArr[idx + 1]) {
+            setVisibleIdx(idx);
+          } else if (current >= offset && idx === allOffsetTopsArr.length - 1) {
+            setVisibleIdx(idx);
+          }
+        });
+      } else {
+        setVisibleIdx(-1);
       }
     }
   });
@@ -133,8 +137,8 @@ export const FloatingContainers = () => {
       {colors.map((color, index) => (
         <Container
           bgColor={color}
-          currentIndex={index}
           isVisible={visibleIdx === index}
+          allOffsetTops={allOffsetTops}
           setAllOffsetTops={setAllOffsetTops}
         />
       ))}
@@ -145,48 +149,43 @@ export const FloatingContainers = () => {
 
 const Container = ({
   bgColor,
-  currentIndex,
   isVisible,
+  allOffsetTops,
   setAllOffsetTops,
 }: {
   bgColor: string;
-  currentIndex: number;
   isVisible: boolean;
-  setAllOffsetTops: (value: number[]) => void;
+  allOffsetTops: Set<number>;
+  setAllOffsetTops: (value: Set<number>) => void;
 }) => {
-  const isFirstIndex = currentIndex === 0;
-  const { ref, offsets } = useOffsets();
+  const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (ref.current && offsets.offsetTop !== 0) {
-      // @ts-expect-error
-      setAllOffsetTops((prev: number[]) => [...prev, offsets.offsetTop]);
+    if (ref.current) {
+      console.log(ref.current.offsetTop);
+      setAllOffsetTops(allOffsetTops.add(ref.current.offsetTop));
     }
   }, [ref.current]);
 
   return (
     <motion.div ref={ref} className={classNames("h-screen w-full", bgColor)}>
-      {!isFirstIndex && (
-        <motion.div
-          initial={{
-            opacity: 1,
-            y: -100,
-          }}
-          animate={{
-            y: isVisible ? 0 : -100,
-            opacity: isVisible ? 1 : 0,
-          }}
-          transition={{
-            duration: 0.2,
-          }}
-          className={classNames(
-            "h-20 border-b border-b-white w-full fixed top-0",
-            bgColor
-          )}
-        ></motion.div>
-      )}
+      <motion.div
+        initial={{
+          opacity: 1,
+          y: -100,
+        }}
+        animate={{
+          y: isVisible ? 0 : -100,
+          opacity: isVisible ? 1 : 0,
+        }}
+        transition={{
+          duration: 0.2,
+        }}
+        className={classNames(
+          "h-20 border-b border-b-white w-full fixed top-0",
+          bgColor
+        )}
+      ></motion.div>
     </motion.div>
   );
 };
-
-// Calculate the offset of current container in viewport from the top of window and when it nears 0, attach the fixed class to it and remove the previous node's fixed class.
