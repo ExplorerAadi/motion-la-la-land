6a0b915dc60b2bf6a478376dad10d92a1756a7e4
Aditya Raj <rajaditya27145@gmail.com>
2024-06-02 23:59:38 +0530

    feat: reveal on scroll

---
+++ components/RevealOnScroll.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { motion, useAnimation, useInView } from "framer-motion";
+import { useEffect, useRef } from "react";
+
+export const RevealOnScroll = ({
+  children,
+  width = "fit-content",
+}: {
+  children: JSX.Element;
+  width?: "fit-content" | "100%";
+}) => {
+  const ref = useRef(null);
+  const inView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      slideControls.start("visible");
+      mainControls.start("visible");
+    }
+  }, [inView, mainControls, slideControls]);
+
+  return (
+    <div ref={ref} style={{ position: "relative", width, overflow: "hidden" }}>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          backgroundColor: "#8b5cf6",
+          zIndex: 20,
+        }}
+      ></motion.div>
+    </div>
+  );
+};
