f9073237c4a43e7128d4ef31426a8cb114c2d32a
Aditya Raj <rajaditya27145@gmail.com>
2024-07-20 18:13:49 +0530

    🎞️ rearrange demos

--- app/Homepage.tsx
+++ app/Homepage.tsx
@@ -18,11 +18,32 @@ export const Homepage = () => {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className="pb-20 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-semibold tracking-tight text-transparent md:text-7xl"
+        className="pb-20 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-semibold tracking-tight text-transparent md:text-7xl mt-28"
       >
         Motion La La Land
       </motion.h1>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-12">
+      <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-12">
+        <Card
+          title="Private Island"
+          description="A private island of links and bio based on the dynamic island concept of iPhones and inspired by @konsfyi design."
+          media={`${cloudinaryDomain}/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/video/upload/f_auto:video,q_auto/private-island-demo`}
+          redirectLabel="Demo"
+          redirectLink="/private-island"
+        />
+        <Card
+          title="Art Gallery"
+          description="An art gallery with images arranged in a masonry grid and an iPhone like opening closing animation."
+          media={`${cloudinaryDomain}/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/video/upload/f_auto:video,q_auto/art-gallery`}
+          redirectLabel="Demo"
+          redirectLink="/modal-animation"
+        />
+        <Card
+          title="Smart Navbar"
+          description="Stick the heading of the section in viewport to the top, super helpful when reading a blog post or a long article."
+          media={`${cloudinaryDomain}/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/video/upload/v1717143294/smart-navbar.mp4`}
+          redirectLabel="Demo"
+          redirectLink="/smart-navbar"
+        />
         <Card
           title="Menu Interaction"
           description="A hamburger menu created by only animating the svg paths and can be used to toggle the open/close state of sidebar menus."
@@ -43,27 +64,6 @@ export const Homepage = () => {
           media={`${cloudinaryDomain}/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/video/upload/v1717143294/mouse-tracking.mp4`}
           redirectLabel="Demo"
           redirectLink="/mask-image-effect"
-        />
-        <Card
-          title="Smart Navbar"
-          description="Stick the heading of the section in viewport to the top, super helpful when reading a blog post or a long article."
-          media={`${cloudinaryDomain}/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/video/upload/v1717143294/smart-navbar.mp4`}
-          redirectLabel="Demo"
-          redirectLink="/smart-navbar"
-        />
-        <Card
-          title="Art Gallery"
-          description="An art gallery with images arranged in a masonry grid and an iPhone like opening closing animation."
-          media={`${cloudinaryDomain}/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/video/upload/f_auto:video,q_auto/art-gallery`}
-          redirectLabel="Demo"
-          redirectLink="/modal-animation"
-        />
-        <Card
-          title="Private Island"
-          description="A private island of links and bio based on the dynamic island concept of iPhones and inspired by the private island of @konsfyi."
-          media={`${cloudinaryDomain}/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/video/upload/f_auto:video,q_auto/private-island-demo`}
-          redirectLabel="Demo"
-          redirectLink="/private-island"
         />
         {/* <RedirectLink label="Custom Animation" href="/custom-animation" /> */}
         {/* <RedirectLink label="Stateless Animation" href="/stateless-animation" /> */}
