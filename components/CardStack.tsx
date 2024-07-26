496f4f14dd4e6b4ce7c01193cf373dbd2b11e396
Aditya Raj <rajaditya27145@gmail.com>
2024-07-26 20:11:51 +0530

    fix cards and breakdown the work to recreate verse

--- components/CardStack.tsx
+++ components/CardStack.tsx
@@ -1,243 +1,32 @@
 "use client";
-import { useEffect, useState, useRef } from "react";
-import { motion } from "framer-motion";
-import { classNames } from "../utils";
 
-let interval: any;
-
-type Card = {
-  id: number;
-  name: string;
-  designation: string;
-  content: React.ReactNode;
-};
-
-export const CardStack = ({
-  items,
-  offset,
-  scaleFactor,
-}: {
-  items: Card[];
-  offset?: number;
-  scaleFactor?: number;
-}) => {
-  const CARD_OFFSET = offset || 10;
-  const SCALE_FACTOR = scaleFactor || 0.06;
-  const [cards, setCards] = useState<Card[]>(items);
-
-  useEffect(() => {
-    startFlipping();
-
-    return () => clearInterval(interval);
-  }, []);
-  const startFlipping = () => {
-    interval = setInterval(() => {
-      setCards((prevCards: Card[]) => {
-        const newArray = [...prevCards]; // create a copy of the array
-        newArray.unshift(newArray.pop()!); // move the last element to the front
-        return newArray;
-      });
-    }, 5000);
-  };
-
+const BaseCard = () => {
   return (
-    <div className="relative  h-60 w-60 md:h-60 md:w-96">
-      {cards.map((card, index) => {
-        return (
-          <motion.div
-            key={card.id}
-            className="absolute dark:bg-black bg-white h-60 w-60 md:h-60 md:w-96 rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-white/[0.1]  shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-between"
-            style={{
-              transformOrigin: "top center",
-            }}
-            animate={{
-              top: index * -CARD_OFFSET,
-              scale: 1 - index * SCALE_FACTOR, // decrease scale for cards that are behind
-              zIndex: cards.length - index, //  decrease z-index for the cards that are behind
-            }}
-          >
-            <div className="font-normal text-neutral-700 dark:text-neutral-200">
-              {card.content}
-            </div>
-            <div>
-              <p className="text-neutral-500 font-medium dark:text-white">
-                {card.name}
-              </p>
-              <p className="text-neutral-400 font-normal dark:text-neutral-200">
-                {card.designation}
-              </p>
-            </div>
-          </motion.div>
-        );
-      })}
+    <div className="flex flex-col items-center justify-center bg-white rounded-lg w-72 h-96 shadow-xl">
+      <h3 className="text-lg">The greatest trick</h3>
     </div>
   );
 };
 
-export function GlareCardDemo() {
+export const BaseCardsStacked = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-      <GlareCard className="flex flex-col items-center justify-center">
-        <svg
-          width="66"
-          height="65"
-          viewBox="0 0 66 65"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-14 w-14 text-white"
-        >
-          <path
-            d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
-            stroke="currentColor"
-            strokeWidth="15"
-            strokeMiterlimit="3.86874"
-            strokeLinecap="round"
-          />
-        </svg>
-      </GlareCard>
-      <GlareCard className="flex flex-col items-center justify-center">
-        <img
-          className="h-full w-full absolute inset-0 object-cover"
-          src="https://images.unsplash.com/photo-1512618831669-521d4b375f5d?q=80&w=3388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
-      </GlareCard>
-      <GlareCard className="flex flex-col items-start justify-end py-8 px-6">
-        <p className="font-bold text-white text-lg">The greatest trick</p>
-        <p className="font-normal text-base text-neutral-200 mt-4">
-          The greatest trick the devil ever pulled was to convince the world
-          that he didn&apos;t exist.
-        </p>
-      </GlareCard>
-    </div>
-  );
-}
-
-export const GlareCard = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  const isPointerInside = useRef(false);
-  const refElement = useRef<HTMLDivElement>(null);
-  const state = useRef({
-    glare: {
-      x: 50,
-      y: 50,
-    },
-    background: {
-      x: 50,
-      y: 50,
-    },
-    rotate: {
-      x: 0,
-      y: 0,
-    },
-  });
-  const containerStyle = {
-    "--m-x": "50%",
-    "--m-y": "50%",
-    "--r-x": "0deg",
-    "--r-y": "0deg",
-    "--bg-x": "50%",
-    "--bg-y": "50%",
-    "--duration": "300ms",
-    "--foil-size": "100%",
-    "--opacity": "0",
-    "--radius": "48px",
-    "--easing": "ease",
-    "--transition": "var(--duration) var(--easing)",
-  } as any;
-
-  const backgroundStyle = {
-    "--step": "5%",
-    "--foil-svg": `url("data:image/svg+xml,%3Csvg width='26' height='26' viewBox='0 0 26 26' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M2.99994 3.419C2.99994 3.419 21.6142 7.43646 22.7921 12.153C23.97 16.8695 3.41838 23.0306 3.41838 23.0306' stroke='white' stroke-width='5' stroke-miterlimit='3.86874' stroke-linecap='round' style='mix-blend-mode:darken'/%3E%3C/svg%3E")`,
-    "--pattern": "var(--foil-svg) center/100% no-repeat",
-    "--rainbow":
-      "repeating-linear-gradient( 0deg,rgb(255,119,115) calc(var(--step) * 1),rgba(255,237,95,1) calc(var(--step) * 2),rgba(168,255,95,1) calc(var(--step) * 3),rgba(131,255,247,1) calc(var(--step) * 4),rgba(120,148,255,1) calc(var(--step) * 5),rgb(216,117,255) calc(var(--step) * 6),rgb(255,119,115) calc(var(--step) * 7) ) 0% var(--bg-y)/200% 700% no-repeat",
-    "--diagonal":
-      "repeating-linear-gradient( 128deg,#0e152e 0%,hsl(180,10%,60%) 3.8%,hsl(180,10%,60%) 4.5%,hsl(180,10%,60%) 5.2%,#0e152e 10%,#0e152e 12% ) var(--bg-x) var(--bg-y)/300% no-repeat",
-    "--shade":
-      "radial-gradient( farthest-corner circle at var(--m-x) var(--m-y),rgba(255,255,255,0.1) 12%,rgba(255,255,255,0.15) 20%,rgba(255,255,255,0.25) 120% ) var(--bg-x) var(--bg-y)/300% no-repeat",
-    backgroundBlendMode: "hue, hue, hue, overlay",
-  };
-
-  const updateStyles = () => {
-    if (refElement.current) {
-      console.log(state.current);
-      const { background, rotate, glare } = state.current;
-      refElement.current?.style.setProperty("--m-x", `${glare.x}%`);
-      refElement.current?.style.setProperty("--m-y", `${glare.y}%`);
-      refElement.current?.style.setProperty("--r-x", `${rotate.x}deg`);
-      refElement.current?.style.setProperty("--r-y", `${rotate.y}deg`);
-      refElement.current?.style.setProperty("--bg-x", `${background.x}%`);
-      refElement.current?.style.setProperty("--bg-y", `${background.y}%`);
-    }
-  };
-  return (
-    <div
-      style={containerStyle}
-      className="relative isolate [contain:layout_style] [perspective:600px] transition-transform duration-[var(--duration)] ease-[var(--easing)] delay-[var(--delay)] will-change-transform w-[320px] [aspect-ratio:17/21]"
-      ref={refElement}
-      onPointerMove={(event) => {
-        const rotateFactor = 0.4;
-        const rect = event.currentTarget.getBoundingClientRect();
-        const position = {
-          x: event.clientX - rect.left,
-          y: event.clientY - rect.top,
-        };
-        const percentage = {
-          x: (100 / rect.width) * position.x,
-          y: (100 / rect.height) * position.y,
-        };
-        const delta = {
-          x: percentage.x - 50,
-          y: percentage.y - 50,
-        };
-
-        const { background, rotate, glare } = state.current;
-        background.x = 50 + percentage.x / 4 - 12.5;
-        background.y = 50 + percentage.y / 3 - 16.67;
-        rotate.x = -(delta.x / 3.5);
-        rotate.y = delta.y / 2;
-        rotate.x *= rotateFactor;
-        rotate.y *= rotateFactor;
-        glare.x = percentage.x;
-        glare.y = percentage.y;
-
-        updateStyles();
-      }}
-      onPointerEnter={() => {
-        isPointerInside.current = true;
-        if (refElement.current) {
-          setTimeout(() => {
-            if (isPointerInside.current) {
-              refElement.current?.style.setProperty("--duration", "0s");
-            }
-          }, 300);
-        }
-      }}
-      onPointerLeave={() => {
-        isPointerInside.current = false;
-        if (refElement.current) {
-          refElement.current.style.removeProperty("--duration");
-          refElement.current?.style.setProperty("--r-x", `0deg`);
-          refElement.current?.style.setProperty("--r-y", `0deg`);
-        }
-      }}
-    >
-      <div className="h-full grid will-change-transform origin-center transition-transform duration-[var(--duration)] ease-[var(--easing)] delay-[var(--delay)] [transform:rotateY(var(--r-x))_rotateX(var(--r-y))] rounded-[var(--radius)] border border-slate-800 hover:[--opacity:0.6] hover:[--duration:200ms] hover:[--easing:linear] hover:filter-none overflow-hidden">
-        <div className="w-full h-full grid [grid-area:1/1] mix-blend-soft-light [clip-path:inset(0_0_0_0_round_var(--radius))]">
-          <div className={classNames("h-full w-full bg-slate-950", className)}>
-            {children}
-          </div>
+    <div className="w-screen h-screen flex items-center justify-center bg-slate-950">
+      <div className="relative h-full w-full">
+        <div className="absolute top-[54%] left-[30%] -translate-x-[30%] -translate-y-[54%]">
+          <BaseCard />
         </div>
-        <div className="w-full h-full grid [grid-area:1/1] mix-blend-soft-light [clip-path:inset(0_0_1px_0_round_var(--radius))] opacity-[var(--opacity)] transition-opacity transition-background duration-[var(--duration)] ease-[var(--easing)] delay-[var(--delay)] will-change-background [background:radial-gradient(farthest-corner_circle_at_var(--m-x)_var(--m-y),_rgba(255,255,255,0.8)_10%,_rgba(255,255,255,0.65)_20%,_rgba(255,255,255,0)_90%)]" />
-        <div
-          className="w-full h-full grid [grid-area:1/1] mix-blend-color-dodge opacity-[var(--opacity)] will-change-background transition-opacity [clip-path:inset(0_0_1px_0_round_var(--radius))] [background-blend-mode:hue_hue_hue_overlay] [background:var(--pattern),_var(--rainbow),_var(--diagonal),_var(--shade)] relative after:content-[''] after:grid-area-[inherit] after:bg-repeat-[inherit] after:bg-attachment-[inherit] after:bg-origin-[inherit] after:bg-clip-[inherit] after:bg-[inherit] after:mix-blend-exclusion after:[background-size:var(--foil-size),_200%_400%,_800%,_200%] after:[background-position:center,_0%_var(--bg-y),_calc(var(--bg-x)*_-1)_calc(var(--bg-y)*_-1),_var(--bg-x)_var(--bg-y)] after:[background-blend-mode:soft-light,_hue,_hard-light]"
-          style={{ ...backgroundStyle }}
-        />
+        <div className="absolute top-[47%] left-[38%] -translate-x-[38%] -translate-y-[47%]">
+          <BaseCard />
+        </div>
+        <div className="absolute top-[42%] left-[47%] -translate-x-[47%] -translate-y-[42%]">
+          <BaseCard />
+        </div>
+        <div className="absolute top-[49%] left-[54%] -translate-x-[54%] -translate-y-[49%]">
+          <BaseCard />
+        </div>
+        <div className="absolute top-[56%] left-[61%] -translate-x-[61%] -translate-y-[56%]">
+          <BaseCard />
+        </div>
       </div>
     </div>
   );
