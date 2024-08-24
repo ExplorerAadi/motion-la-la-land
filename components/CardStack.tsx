f4fea53572d58c2a76f609fc565dc1d5ae937daa
Aditya Raj <rajaditya27145@gmail.com>
2024-08-24 23:42:47 +0530

    ✨ polish animation

--- components/CardStack.tsx
+++ components/CardStack.tsx
@@ -1,123 +1,9 @@
 "use client";
 
-import {
-  motion,
-  useAnimate,
-  useMotionValue,
-  useMotionValueEvent,
-} from "framer-motion";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useWindowSize } from "usehooks-ts";
-import { classNames } from "../utils";
-
-const BaseCard = ({
-  index,
-  position,
-  width,
-  height,
-  flippedCardIdx,
-  setFlippedCardIdx,
-}: {
-  index: number;
-  position: { x: number; y: number };
-  width: number;
-  height: number;
-  flippedCardIdx: number;
-  setFlippedCardIdx: React.Dispatch<React.SetStateAction<number>>;
-}) => {
-  const x = useMotionValue(position.x);
-  const y = useMotionValue(position.y);
-  const [isAnimationComplete, setIsAnimationComplete] = useState(false);
-  const isFlipped = flippedCardIdx === index;
-
-  const [scope, animate] = useAnimate();
-
-  useEffect(() => {
-    animate(
-      scope.current,
-      {
-        opacity: 1,
-        rotateY: isFlipped ? 180 : 0,
-        scale: isFlipped ? 2.5 : 1,
-        zIndex: isFlipped ? 999 : undefined,
-        x: isFlipped ? width / 2 - 100 : x.get(),
-        y: isFlipped ? height / 2 - 150 : y.get(),
-      },
-      {
-        duration: 0.2,
-        type: "spring",
-        stiffness: 120,
-        delay: index > 0 ? 0.2 * index : 0,
-      }
-    );
-    animate("h3", { rotateY: isFlipped ? -180 : 0 });
-  }, [isFlipped]);
-
-  useMotionValueEvent(x, "change", () => {
-    animate(
-      scope.current,
-      { x },
-      { type: "spring", stiffness: 1000, damping: 10 }
-    );
-  });
-
-  useMotionValueEvent(y, "change", () => {
-    animate(
-      scope.current,
-      { y },
-      { type: "spring", stiffness: 1000, damping: 10 }
-    );
-  });
-
-  return (
-    <motion.button
-      ref={scope}
-      drag={true}
-      dragMomentum={false}
-      dragConstraints={{
-        left: 10,
-        right: width - 300,
-        top: 10,
-        bottom: height - 400,
-      }}
-      className={classNames(
-        "flex flex-col items-center justify-center bg-white rounded-lg w-72 h-96 shadow-xl",
-        isFlipped ? "cursor-grabbing" : "cursor-pointer"
-      )}
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-      }}
-      initial={{
-        opacity: 0,
-        x: width / 2,
-        y: height + 100,
-      }}
-      onMouseDown={() => (window.xyValue = { x: x.get(), y: y.get() })}
-      onMouseUp={() => {
-        if (
-          Math.abs(x.get() - window.xyValue.x) < 2 ||
-          Math.abs(y.get() - window.xyValue.y) < 2
-        ) {
-          setFlippedCardIdx(index);
-        }
-      }}
-      onDrag={(_, info) => {
-        if (info.point.x > 200 || info.point.y > 200) {
-          x.set(info.point.x);
-          y.set(info.point.y);
-        }
-      }}
-      onAnimationComplete={() => setIsAnimationComplete(true)}
-      onAnimationStart={() => setIsAnimationComplete(false)}
-    >
-      <motion.h3 className="text-lg" initial={{}}>
-        {isAnimationComplete && isFlipped ? "Got you!" : "The greatest trick"}
-      </motion.h3>
-    </motion.button>
-  );
-};
+import { DeckCard } from "./DeckCard";
+import { CloseIcon } from "../public/icons/CloseIcon";
 
 export const BaseCardsStacked = () => {
   const { width, height } = useWindowSize();
@@ -131,9 +17,19 @@ export const BaseCardsStacked = () => {
   };
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-slate-950 overflow-hidden relative">
+    <div
+      className="w-screen h-screen flex items-center justify-center bg-slate-950 overflow-hidden relative"
+      onClick={(e) => {
+        if (
+          !(e.target as HTMLElement).classList.contains("deck-card") &&
+          flippedCardIdx !== -1
+        ) {
+          setFlippedCardIdx(-1);
+        }
+      }}
+    >
       {positions.map((position, idx) => (
-        <BaseCard
+        <DeckCard
           key={idx}
           index={idx}
           position={{ x: getX(position.x), y: getY(position.y) }}
@@ -143,6 +39,9 @@ export const BaseCardsStacked = () => {
           setFlippedCardIdx={setFlippedCardIdx}
         />
       ))}
+      <button className="absolute z-[999] bg-neutral-800 text-white rounded-full p-3 left-1/2 top-8 -translate-x-1/2 -translate-y-1/2 cursor-pointer">
+        <CloseIcon className="h-6 font-semibold" />
+      </button>
     </div>
   );
 };
---
+++ components/DeckCard.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import {
+  motion,
+  useAnimate,
+  useMotionValue,
+  useMotionValueEvent,
+  useSpring,
+} from "framer-motion";
+import { useEffect, useState } from "react";
+import { classNames } from "../utils";
+
+export const DeckCard = ({
+  index,
+  position,
+  width,
+  height,
+  flippedCardIdx,
+  setFlippedCardIdx,
+}: {
+  index: number;
+  position: { x: number; y: number };
+  width: number;
+  height: number;
+  flippedCardIdx: number;
+  setFlippedCardIdx: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+  const x = useMotionValue(position.x);
+  const y = useMotionValue(position.y);
+  const [isAnimationComplete, setIsAnimationComplete] = useState(false);
+  const isFlipped = flippedCardIdx === index;
+
+  const [scope, animate] = useAnimate();
+
+  const springConfig = { damping: 10, stiffness: 120 };
+  const dragControls = useSpring(x, springConfig);
+  const dragControlsY = useSpring(y, springConfig);
+
+  useEffect(() => {
+    animate(
+      scope.current,
+      {
+        opacity: 1,
+        rotateY: isFlipped ? 180 : 0,
+        scale: isFlipped ? 1.8 : 1,
+        x: isFlipped ? width / 2 - 100 : x.get(),
+        y: isFlipped ? height / 2 - 150 : y.get(),
+      },
+      {
+        duration: 0.2,
+        type: "spring",
+        stiffness: 90,
+        // delay: index > 0 ? 0.2 * index : 0,
+      }
+    );
+    animate("h3", { rotateY: isFlipped ? -180 : 0 });
+  }, [isFlipped]);
+
+  useMotionValueEvent(x, "change", () => {
+    animate(
+      scope.current,
+      { x },
+      { type: "spring", stiffness: 1000, damping: 10, delay: 0.2 }
+    );
+  });
+
+  useMotionValueEvent(y, "change", () => {
+    animate(
+      scope.current,
+      { y },
+      { type: "spring", stiffness: 1000, damping: 10, delay: 0.2 }
+    );
+  });
+
+  return (
+    <motion.button
+      ref={scope}
+      drag={true}
+      dragElastic={0.1}
+      dragTransition={{ power: 0.2, timeConstant: 120 }}
+      dragMomentum={false}
+      dragConstraints={{
+        left: 10,
+        right: width - 300,
+        top: 10,
+        bottom: height - 400,
+      }}
+      className={classNames(
+        "flex flex-col items-center justify-center bg-white rounded-lg w-72 h-96 shadow-xl deck-card absolute top-0 left-0",
+        x.isAnimating() || y.isAnimating()
+          ? "cursor-grabbing"
+          : "cursor-pointer"
+      )}
+      style={{
+        x: dragControls,
+        y: dragControlsY,
+        zIndex: isFlipped ? 999 : "auto",
+      }}
+      initial={{
+        opacity: 0,
+        x: width / 2,
+        y: height + 100,
+      }}
+      onMouseDown={(e) => {
+        e.stopPropagation();
+        window.xyValue = { x: x.get(), y: y.get() };
+      }}
+      onMouseUp={(e) => {
+        e.stopPropagation();
+        if (
+          Math.abs(x.get() - window.xyValue.x) < 2 ||
+          Math.abs(y.get() - window.xyValue.y) < 2
+        ) {
+          setFlippedCardIdx(index);
+        }
+      }}
+      onDrag={(_, info) => {
+        if (info.point.x > 200 || info.point.y > 200) {
+          dragControls.set(info.point.x);
+          dragControlsY.set(info.point.y);
+        }
+      }}
+      onAnimationComplete={() => setIsAnimationComplete(true)}
+      onAnimationStart={() => setIsAnimationComplete(false)}
+    >
+      <motion.h3 className="text-lg deck-card" initial={{}}>
+        {isAnimationComplete && isFlipped ? "Got you!" : "The greatest trick"}
+      </motion.h3>
+    </motion.button>
+  );
+};
